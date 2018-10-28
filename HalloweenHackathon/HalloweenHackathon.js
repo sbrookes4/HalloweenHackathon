84f30ba5d47b80e5574d5dfb767d104808c77a7c
Kendra <kendra@coffeeringsdesign.com>
2018-10-27 17:43:43 -0700

    js for map

--- HalloweenHackathon/HalloweenHackathon.js
+++ HalloweenHackathon/HalloweenHackathon.js
@@ -14,7 +14,7 @@ $(document).ready(function() {
         genreArray = genres.split(",")
         for (i=0; i<genreArray.length; i++) {
         // genreArray.forEach(function(genre) {
-          if (genreArray[i] === " Horror") {
+          if ((genreArray[i] === " Horror") || (genreArray[i] === "Horror")) {
 
             $(".showTitle").append(`<div class='card showMovie' style='width: 18rem;' 'id=movie${i}'>
                       <div class='card-body'>
@@ -33,4 +33,28 @@ $(document).ready(function() {
       }
     })
   });
+  //mapbox token to access its api
+  mapboxgl.accessToken = "pk.eyJ1Ijoia3NlbGxlcnMiLCJhIjoiY2lteTd1Nm9lMDN6bXY3a2toZGY5YWVteSJ9.R_LBBQrD6gwBAH-Aq93ZUw";
+  // replace this with your access token
+
+  //This limits the full extent of the map
+  var bounds = [
+    [-180.679, -80.60], // Southwest coordinates
+    [180.187, 84.549]  // Northeast coordinates
+   // [-180.679, -80.60], // Southwest coordinates
+   // [180.187, 84.549]  // Northeast coordinates
+  ];
+
+  //adding my mapbox style
+  var map = new mapboxgl.Map({
+   container: "map", // container id
+   style: "mapbox://styles/ksellers/cjnrwl9qn2dpg2skzqi7auqzk",
+   center: [0, 30], // starting position
+   zoom: 1.25, // starting zoom
+   maxBounds: bounds
+  });
+
+
+  // Add zoom and rotation controls to the map.
+  map.addControl(new mapboxgl.NavigationControl());
 });
